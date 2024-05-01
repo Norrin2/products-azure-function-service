e8f337dd54f2f84410a15550e43130e6201570c0
Rafael Santos <rafael_dos_santos_25@hotmail.com>
2024-05-01 14:19:07 -0300

    initial commit

---
+++ ProductsFunctionApp/ProductsFunctionApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsFunctionApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+    }
+}
